1610dcea746800af1854cccca1aec147430d7bf4
二六壹十贰 <529729644@qq.com>
2022-12-02 07:55:19 +0000

    注释掉不能运行部分

--- 1_C_Start/main.c
+++ 1_C_Start/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
+// #include <stdlib.h>
 
 int main() {
     printf("Hello World\n");
-    system("pause");// 旧版IDE需要这行让黑色控制台窗口停留
+    // system("pause");// 旧版IDE需要这行让黑色控制台窗口停留
     return 0;
 }
