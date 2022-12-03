72efb600827ab93a1d67ad02c31aab449660632d
二六壹十贰 <529729644@qq.com>
2022-12-03 18:32:44 +0000

    符号常量

--- 1_C_Start/main.c
+++ 1_C_Start/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 // #include <stdlib.h>
 
+#define PI 3
+
 int main()
 {
     printf("Hello World\n");
     // system("pause");// 旧版IDE需要这行让黑色控制台窗口停留
+
+    // PI = 5;// 符号常量不可赋值
+    printf("%d\n", PI);
+
     return 0;
 }
